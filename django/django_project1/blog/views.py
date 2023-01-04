472225b2e040cffe0cff875b0e19fe2739902b51
AmarSabbisetti <amarnadh.s@consultadd.com>
2023-01-04 14:10:58 -0500

    new file:   .DS_Store 	new file:   django/.DS_Store 	new file:   django/1.py 	new file:   django/django_project1/.DS_Store 	new file:   django/django_project1/blog/.DS_Store 	new file:   django/django_project1/blog/TRACK_BLOG.txt 	new file:   django/django_project1/blog/__init__.py 	new file:   django/django_project1/blog/__pycache__/__init__.cpython-39.pyc 	new file:   django/django_project1/blog/__pycache__/admin.cpython-39.pyc 	new file:   django/django_project1/blog/__pycache__/apps.cpython-39.pyc 	new file:   django/django_project1/blog/__pycache__/models.cpython-39.pyc 	new file:   django/django_project1/blog/__pycache__/urls.cpython-39.pyc 	new file:   django/django_project1/blog/__pycache__/views.cpython-39.pyc 	new file:   django/django_project1/blog/admin.py 	new file:   django/django_project1/blog/apps.py 	new file:   django/django_project1/blog/migrations/__init__.py 	new file:   django/django_project1/blog/migrations/__pycache__/__init__.cpython-39.pyc 	new file:   django/django_project1/blog/models.py 	new file:   django/django_project1/blog/static/blog/main.css 	new file:   django/django_project1/blog/templates/.DS_Store 	new file:   django/django_project1/blog/templates/blog/about.html 	new file:   django/django_project1/blog/templates/blog/base.html 	new file:   django/django_project1/blog/templates/blog/home.html 	new file:   django/django_project1/blog/tests.py 	new file:   django/django_project1/blog/urls.py 	new file:   django/django_project1/blog/views.py 	new file:   django/django_project1/db.sqlite3 	new file:   django/django_project1/django_project1/__init__.py 	new file:   django/django_project1/django_project1/__pycache__/__init__.cpython-39.pyc 	new file:   django/django_project1/django_project1/__pycache__/settings.cpython-39.pyc 	new file:   django/django_project1/django_project1/__pycache__/urls.cpython-39.pyc 	new file:   django/django_project1/django_project1/__pycache__/wsgi.cpython-39.pyc 	new file:   django/django_project1/django_project1/asgi.py 	new file:   django/django_project1/django_project1/settings.py 	new file:   django/django_project1/django_project1/urls.py 	new file:   django/django_project1/django_project1/wsgi.py 	new file:   django/django_project1/manage.py

---
+++ django/django_project1/blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+## we are trying to pass data to templates.
+posts=[
+    {
+        'author':'Amar',
+        'title': 'Blog Post',
+        'content': 'First post',
+        'date_posted':'January 01, 2023'
+    },
+    {
+        'author':'Harika',
+        'title': 'Blog Post 2',
+        'content': 'First post',
+        'date_posted':'January 01, 2023'
+    }
+]
+## we post this above data passing it as argument with our data
+
+def home(request):
+    ##Our data
+    context={
+        'posts': posts
+    }
+    #return HttpResponse('<h1>Blog Home</h1>') ---if we want to pass html code.
+    ##if we want to render html file we have to create in temoplates/<app_name> the we use render function 
+    ##which is already in django.shortcuts
+    return render(request,'blog/home.html', context)
+
+def about(request):
+    #return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html',{'title':'About'})
