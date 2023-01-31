9e0e070010be065d4210c724ebf644b60fa9ecc3
AmarSabbisetti <amarnadh.s@consultadd.com>
2023-01-31 17:05:04 -0500

    Users

--- django/django_project1/blog/models.py
+++ django/django_project1/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+#class in django means one table in database
+class Post(models.Model):
+    #create attributes in database like title
+    title=models.CharField(max_length=100)
+    content= models.TextField()
+    data_posted=models.DateTimeField(default=timezone.now) #here we use default because of timezone are different for that we imported timezone from django.utilis
+    author= models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- django/django_project1/blog/views.py
+++ django/django_project1/blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+#importing Tables in DB
+from .models import Post
 # Create your views here.
 
-## we are trying to pass data to templates.
-posts=[
+## we are trying to pass dummy data to templates.
+'''posts=[
     {
         'author':'Amar',
         'title': 'Blog Post',
@@ -16,13 +19,13 @@ posts=[
         'content': 'First post',
         'date_posted':'January 01, 2023'
     }
-]
+]'''
 ## we post this above data passing it as argument with our data
 
 def home(request):
     ##Our data
     context={
-        'posts': posts
+        'posts': Post.objects.all()
     }
     #return HttpResponse('<h1>Blog Home</h1>') ---if we want to pass html code.
     ##if we want to render html file we have to create in temoplates/<app_name> the we use render function 
